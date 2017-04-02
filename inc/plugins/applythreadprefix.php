8679c3e3c50d5fae93dd2ccc22528de8a3e8a64d
PaulBender <paulbender@live.com>
2017-04-02 14:48:43 -0400

    Use THIS_SCRIPT for template caching

--- inc/plugins/applythreadprefix.php
+++ inc/plugins/applythreadprefix.php
@@ -11,7 +11,7 @@ if(!defined("IN_MYBB"))
 }
 
 // Neat trick for caching our custom template(s)
-if(my_strpos($_SERVER['PHP_SELF'], 'showthread.php'))
+if(THIS_SCRIPT == 'showthread.php')
 {
 	global $templatelist;
 	if(isset($templatelist))
@@ -21,7 +21,7 @@ if(my_strpos($_SERVER['PHP_SELF'], 'showthread.php'))
 	$templatelist .= 'showthread_moderationoptions_applyprefix';
 }
 
-if(my_strpos($_SERVER['PHP_SELF'], 'forumdisplay.php'))
+if(THIS_SCRIPT == 'forumdisplay.php')
 {
 	global $templatelist;
 	if(isset($templatelist))
