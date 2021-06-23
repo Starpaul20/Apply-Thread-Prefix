b79e7fa42473ec8fb2321615b9d80e640995ea31
Paul Bender <paulbender@live.com>
2021-06-23 11:15:26 -0400

    Bump to version v1.3

--- inc/plugins/applythreadprefix.php
+++ inc/plugins/applythreadprefix.php
@@ -51,7 +51,7 @@ function applythreadprefix_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.2",
+		"version"			=> "1.3",
 		"codename"			=> "applythreadprefix",
 		"compatibility"		=> "18*"
 	);
